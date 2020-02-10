28ed3eab5d30f72d38c7bcaed1521ad856f4b125
Jabrils <ankelzspam@gmail.com>
2020-02-09 21:10:23 -0800

    v0.0.2
    we're almost done with A*

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -10,15 +10,27 @@ public class Map
     public int mapSize => _mapSize;
     public int halfMapSize => _mapSize / 2;
     public Tile a => current;
-    Tile current;
+    Tile tileFrom => current.parent == null ? current.parent : current;
+    Tile tileTo => pos[tX, tY];
+    int fX, fY, tX, tY;
+    public TilePath thee = new TilePath(new List<Tile>());
 
+    Tile current;
     Tile[,] pos;
-    List<Tile> openTiles, closedTiles;
+    public List<Tile> openTiles, closedTiles;
 
     public Map(int size)
     {
         pos = new Tile[size, size];
         _mapSize = size;
+    }
+
+    public void BlockTiles(Vector2Int[] b)
+    {
+        for (int i = 0; i < b.Length; i++)
+        {
+            pos[b[i].x, b[i].y].free = false;
+        }
     }
 
     public void Assign()
@@ -31,164 +43,226 @@ public class Map
                 pos[i, j] = new Tile(i, j);
             }
         }
+    }
 
-        // then we set the neighbors
+    public IEnumerator FindPath(float t, int _fX, int _fY, int _tX, int _tY)
+    {
+        fX = _fX;
+        fY = _fY;
+        tX = _tX;
+        tY = _tY;
+
+        BlockTiles(GameObject.Find("CTRL").GetComponent<ctrl>().block);
+
+        // first we set the neighbors
         for (int i = 0; i < mapSize; i++)
         {
             for (int j = 0; j < mapSize; j++)
             {
-                pos[i, j].SetNeighbors(GetNeighbors(pos[i, j]));
+                //pos[i, j].SetNeighbors(GetNeighbors(pos[i, j], pos[fX, fY], pos[tX, tY]));
             }
         }
-    }
 
-    public void FindPath(int fX, int fY, int tX, int tY)
-    {
         // set list of tiles to be evaluated
         openTiles = new List<Tile>();
         // set list of tiles already evaluated
         closedTiles = new List<Tile>();
 
         // add current node to the open list
-        openTiles.Add(pos[fX, fY]);
+        AddTileToOpen(pos[fX, fY]);
 
         // 
         while (openTiles.Count > 0)
         {
-            // current tile = tile with the lowest f cost
-            current = openTiles[0];
-
-            // 
-            current = GetLowestFCost(current);
-
-            // remove current from open
-            openTiles.Remove(current);
-
-            // add current to closed
-            closedTiles.Add(current);
+            // current tile = tile with the lowest f cost in the open set (start off with only 1 tile in the open tiles list so that is the lowest)
+            current = GetLowestFCost();
 
             // if current == target, then we're done
             if (current == pos[tX, tY])
             {
                 Debug.Log("AT END!");
 
+                    thee._path.Add(current);
+
+                while (current != pos[fX, fY])
+                {
+                    thee._path.Add(current.parent);
+                    current = current.parent;
+                }
+
                 break;
             }
 
-            // For testing
-            for (int i = 0; i < current.neighbors.Count; i++)
-            {
-                Debug.Log($"{current.neighbors[i].loc}");
-            }
+            // if not then well Process this tile for exploration
+            ProcessTile(current);
 
-            // 
-            foreach (Tile n in current.neighbors)
-            {
-                // if the neighbor is occupied or if the neighbor is in the closed list,
-                if (n.occupied || closedTiles.Contains(n))
-                {
-                    // skip it
-                    continue;
-                }
-
-                // if new path to neighbor is shorter, or neighbor is not in open
-                if (false)
-                {
-                    // set f cost of neighbor
-                    // set parent of neighbor to current
-
-                    // if neighbor is not in open
-                    // add neighbor to open
-                }
-            }
+            yield return new WaitForSeconds(t);
         }
     }
 
-    List<Tile> GetNeighbors(Tile current)
+    void AddTileToOpen(Tile t)
     {
+        openTiles.Add(t);
+    }
+
+    void ProcessTile(Tile c)
+    {
+        // Get & set all of the neighbors for that tile, which will also add it to the open tiles list
+        current.SetNeighbors(GetNeighbors(current, current.parent, tileTo));
+
+        // remove current from open since it's ben processed
+        openTiles.Remove(c);
+
+        // add current to closed since its been processed
+        closedTiles.Add(c);
+    }
+
+    float GetDistance(Vector2 a, Vector2 b)
+    {
+        return (a - b).sqrMagnitude;
+    }
+
+    List<Tile> GetNeighbors(Tile current, Tile _from, Tile to)
+    {
+        _from = current;
+
         List<Tile> ret = new List<Tile>();
 
-        // 
-        if (current.y - 1 >= 0)
+        // Set our current tile's fCost
+        float gc = GetDistance(new Vector2(current.x, current.y), new Vector2(_from.x, _from.y));
+        float hc = GetDistance(new Vector2(current.x, current.y), new Vector2(to.x, to.y));
+
+        current.SetGnH(gc, hc);
+
+        Tile n = pos[current.x - 1, current.y];
+        // set our neighbor's fcost
+        if (current.x - 1 >= 0 && !closedTiles.Contains(n) && pos[current.x - 1, current.y].free)
         {
-            ret.Add(pos[current.x, current.y - 1]);
+            gc = GetDistance(new Vector2(n.x, n.y), new Vector2(_from.x, _from.y));
+            hc = GetDistance(new Vector2(n.x, n.y), new Vector2(to.x, to.y));
+
+            n.SetGnH(gc, hc);
+            n.SetParent(current);
+            AddTileToOpen(n);
+            ret.Add(n);
+            //Debug.Log($"NEI: {n.dist} - {n.fCost}");
         }
 
+        n = pos[current.x, current.y - 1];
         // 
-        if (current.x + 1 < mapSize)
+        if (current.y - 1 >= 0 && !closedTiles.Contains(n) && pos[current.x, current.y - 1].free)
         {
-            ret.Add(pos[current.x + 1, current.y]);
+            gc = GetDistance(new Vector2(n.x, n.y), new Vector2(_from.x, _from.y));
+            hc = GetDistance(new Vector2(n.x, n.y), new Vector2(to.x, to.y));
+
+            n.SetGnH(gc, hc);
+            n.SetParent(current);
+            AddTileToOpen(n);
+            ret.Add(n);
+            //Debug.Log($"NEI: {n.dist} - {n.fCost}");
         }
 
+        n = pos[current.x + 1, current.y];
         // 
-        if (current.y + 1 < mapSize)
+        if (current.x + 1 < mapSize && !closedTiles.Contains(n) && pos[current.x + 1, current.y].free)
         {
-            ret.Add(pos[current.x, current.y + 1]);
+            gc = GetDistance(new Vector2(n.x, n.y), new Vector2(_from.x, _from.y));
+            hc = GetDistance(new Vector2(n.x, n.y), new Vector2(to.x, to.y));
+
+            n.SetGnH(gc, hc);
+            n.SetParent(current);
+            AddTileToOpen(n);
+            ret.Add(n);
+            //Debug.Log($"NEI: {n.dist} - {n.fCost}");
         }
 
+        n = pos[current.x, current.y + 1];
         // 
-        if (current.x - 1 >= 0)
+        if (current.y + 1 < mapSize && !closedTiles.Contains(n) && pos[current.x, current.y + 1].free)
         {
-            ret.Add(pos[current.x - 1, current.y]);
+            gc = GetDistance(new Vector2(n.x, n.y), new Vector2(_from.x, _from.y));
+            hc = GetDistance(new Vector2(n.x, n.y), new Vector2(to.x, to.y));
+
+            n.SetGnH(gc, hc);
+            n.SetParent(current);
+            AddTileToOpen(n);
+            ret.Add(n);
+            //Debug.Log($"NEI: {n.dist} - {n.fCost}");
         }
 
         return ret;
     }
 
-    Tile GetLowestFCost(Tile currentTile)
+    Tile GetLowestFCost()
     {
+        // 
+        Tile lowest = new Tile(0, 0);
+        lowest.SetGnH(0, Mathf.Infinity);
+
+        // 
         for (int i = 0; i < openTiles.Count; i++)
         {
-            // if they are the same tile,
-            if (openTiles[i] == currentTile)
+            if (openTiles[i].fCost < lowest.fCost)
             {
-                // skip it & dont waste time.
-                continue;
-            }
-
-            // if the looped tile is cheaper than our current tile,
-            // OR
-            // the looped tile & the current node cost the same, && the looped tile has a lower hCost than the current tile
-            if (openTiles[i].fCost < currentTile.fCost || (openTiles[i].fCost == currentTile.fCost && openTiles[i].hCost < currentTile.hCost))
-            {
-                // switch our current tile to that looped tile
-                currentTile = openTiles[i];
+                lowest = openTiles[i];
             }
         }
 
-        return currentTile;
+        // 
+        return lowest;
     }
 }
 
 public class Tile
 {
     //public Tile[] neighbor = new Tile[4];
-    public bool occupied;
+    public bool free = true;
 
     public int x => _x;
     public int y => _y;
     public float fCost => gCost + hCost;
     public string loc => $"{x}, {y}";
+    public string dist => $"{gCost}, {hCost}";
     public List<Tile> neighbors => _neighbors;
-    public float gCost, hCost; // g = dist from start pos; h = dis from end;
+    public float gCost => _gCost;
+    public float hCost => _hCost; // g = dist from start pos; h = dis from end;
+    public Tile parent => _parent;
 
-    Tile parent;
+    Tile _parent;
     List<Tile> _neighbors;
     bool open, closed;
     int _x, _y;
+    float _gCost, _hCost;
 
     public Tile(int X, int Y)
     {
         _x = X;
         _y = Y;
-
     }
 
     public void SetNeighbors(List<Tile> N)
     {
-        if (_neighbors == null)
-        {
-            _neighbors = N;
-        }
+        _neighbors = N;
+    }
+
+    public void SetGnH(float g, float h)
+    {
+        _gCost = g;
+        _hCost = h;
+    }
+
+    public void SetParent(Tile p)
+    {
+        _parent = p;
+    }
+}
+
+public struct TilePath
+{
+    public List<Tile> _path;
+
+    public TilePath(List<Tile> p)
+    {
+        _path = p;
     }
 }
--- Assets/Scripts/ctrl.cs
+++ Assets/Scripts/ctrl.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ctrl : MonoBehaviour
 {
+    public float time;
     public Vector2Int from, to;
+    public Vector2Int[] block;
     Map map;
 
     // Start is called before the first frame update
@@ -12,7 +14,8 @@ public class ctrl : MonoBehaviour
     {
         map = new Map(20);
         map.Assign();
-        map.FindPath(from.x, from.y, to.x, to.y);
+
+        StartCoroutine(map.FindPath(time, from.x, from.y, to.x, to.y));
     }
 
     // Update is called once per frame
@@ -23,21 +26,43 @@ public class ctrl : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        if (Application.isPlaying)
+        {
+            Gizmos.color = Color.yellow;
+
+            // 
+            for (int i = 0; i < map.openTiles.Count; i++)
+            {
+                Gizmos.DrawCube(new Vector3(map.openTiles[i].x, 1, map.openTiles[i].y), Vector3.one);
+            }
+
+            Gizmos.color = Color.blue;
+
+            // 
+            for (int i = 0; i < map.closedTiles.Count; i++)
+            {
+                Gizmos.DrawCube(new Vector3(map.closedTiles[i].x, 1, map.closedTiles[i].y), Vector3.one);
+            }
+
+            Gizmos.color = Color.cyan;
+
+            for (int i = 0; i < map.thee._path.Count; i++)
+            {
+                Gizmos.DrawCube(new Vector3(map.closedTiles[i].x, 1, map.closedTiles[i].y), Vector3.one);
+            }
+
+        }
+
         Gizmos.color = Color.green;
         Gizmos.DrawCube(new Vector3(from.x, 1, from.y), Vector3.one);
 
         Gizmos.color = Color.red;
         Gizmos.DrawCube(new Vector3(to.x, 1, to.y), Vector3.one);
 
-        if (Application.isPlaying)
+        foreach (Vector2Int ve in block)
         {
-            Gizmos.color = Color.yellow;
-
-            // 
-            for (int i = 0; i < map.a.neighbors.Count; i++)
-            {
-                Gizmos.DrawCube(new Vector3(map.a.neighbors[i].x, 1, map.a.neighbors[i].y), Vector3.one);
-            }
+            Gizmos.color = Color.black;
+            Gizmos.DrawCube(new Vector3(ve.x, 1, ve.y), Vector3.one);
         }
     }
 }
